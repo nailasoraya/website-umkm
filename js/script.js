6c93c696126ffdf01c7762ab67881cb7444e4376
nailasoraya <nailasoraya0714@gmail.com>
2026-09-14 13:44:00 +0700

    feat: final website UMKM

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ const promoButton = document.querySelector("#promoButton");
 
 promoButton.addEventListener("click", () => {
     promoButton.textContent = "promo. Beli 2 gratis tester!";
-    console.log("Promo Kopi Nusa berhasil ditampilkan");
+    console.log("Promo Secangklir Jeda berhasil ditampilkan");
 });
